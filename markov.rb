ed15c4e7898e112ecf8f3801eccfd01decd007ec
caneroj1 <caneroj1@tcnj.edu>
2014-06-20 23:51:12 -0400

    first commit with working code.

---
+++ markov.rb
@@ -0,0 +1,69 @@
+module Markov
+  class Generator
+    attr_accessor :corpus, :look_ahead, :words
+    attr_reader :probability_table
+
+    def initialize(corpus, look_ahead, words_to_make)
+      @probability_table = {}
+      @corpus, @look_ahead, @words = corpus, look_ahead, words_to_make
+      generate_table
+      populate_table
+    end
+
+    def generate_text
+      result = String.new
+      word_hash = @probability_table.to_a.sample(1).to_h
+
+      word = word_hash.keys[0]
+      result << word
+
+      @words.times do
+        break if word_hash[word].nil?
+        rand_word = word_from_weighted_hash(word_hash[word])
+        result << rand_word
+        new_hash = @probability_table[rand_word]
+        word_hash = {rand_word => new_hash}
+        word = word_hash.keys[0]
+      end
+      result
+    end
+
+    private
+    def generate_table
+      corpus.scan(/.{#{@look_ahead}}/).each do |word|
+        @probability_table[word] = {} unless @probability_table.has_key? word
+      end 
+    end
+
+    def populate_table
+      corpus_index = 0
+      while (corpus_index < corpus.length - @look_ahead)
+        first_word = @corpus[corpus_index...corpus_index + @look_ahead]
+        follower = @corpus[corpus_index + @look_ahead...corpus_index + 2 * @look_ahead]
+        if @probability_table[first_word].has_key? follower
+          @probability_table[first_word][follower] += 1
+        else
+          @probability_table[first_word][follower] = 1
+        end
+        corpus_index += @look_ahead
+      end
+    end
+
+    def word_from_weighted_hash(hash)
+      result = ""
+      sum = hash.each_value.inject { |value, sum| sum += value }
+      random_weight = rand(1..sum)
+      loop do
+        pair = hash.to_a.sample(1).to_h
+        val = pair.values[0]
+        if val > random_weight
+          result = pair.keys[0]
+          break 
+        else
+          random_weight -= val
+        end
+      end
+      result
+    end
+  end
+end
---
+++ test.rb
@@ -0,0 +1,7 @@
+require_relative 'markov.rb'
+
+
+text = %q{Sgt. Pepper's Lonely Hearts Club Band is the eighth studio album by the English rock band the Beatles (pictured). Released on 1 June 1967, it was an immediate commercial and critical success. After the group retired from touring, Paul McCartney had an idea for a song involving an Edwardian era military band, and this developed into a plan to release an entire album as a performance by the fictional Sgt. Pepper band. Knowing they would not have to perform the tracks live, the Beatles adopted an experimental approach to composition, writing songs such as "With a Little Help from My Friends", "Lucy in the Sky with Diamonds" and "A Day in the Life". The producer George Martin's innovative recording of the album included the liberal application of signal processing. The cover, depicting the band in front of a collage of celebrities and historical figures, was designed by the English pop artists Peter Blake and Jann Haworth. One of the best-selling albums of all time, Sgt. Pepper is regarded as an important work of British psychedelia and an early concept album. One music scholar has described it as "the most important and influential rock and roll album ever recorded}
+
+generator = Markov::Generator.new(text, 1, 100)
+puts generator.generate_text
